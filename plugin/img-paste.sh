2d91e8ad1ff055ddeb4f482e7633f1cf3048f516
loblab <11693722+loblab@users.noreply.github.com>
2024-01-20 15:27:34 +0800

    use new approach for wsl_paste; use msg constant

--- plugin/img-paste.sh
+++ plugin/img-paste.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="img-paste.sh ver 0.2.2 (01/20/2024)"
+VERSION="img-paste.sh ver 0.2.4 (01/20/2024)"
 set -eu -o pipefail
 
 function usage() {
@@ -13,8 +13,12 @@ Usage:
 }
 
 function config() {
+    readonly RC_NOIMGDATA=3
+    readonly MSG_NOIMGDATA="No image data in clipboard."
+
     test $# -gt 0 || usage
     OUT_FILE=$1
+
     which wslpath >/dev/null 2>&1 && PROC=wsl_paste && return 0
     test -v WAYLAND_DISPLAY && which wl-paste >/dev/null 2>&1 && PROC=wl_paste && return 0
     test -v DISPLAY && which xclip >/dev/null 2>&1 && PROC=xclip_paste && return 0
@@ -33,41 +37,36 @@ function cleanup() {
     test -s $OUT_FILE || rm -f $OUT_FILE
 }
 
-function brief_error() {
-    echo "$ERRMSG" | grep -iP "error|fail|warn" | tail -n 1 >&2 ||\
-    echo "$ERRMSG" | grep -P ".{20,80}" | tail -n 1 >&2
-}
-
 function wsl_paste() {
     # wslpath in old WSL needs existance of $OUT_FILE
-    touch $OUT_FILE
     local outfile=$(wslpath -w $OUT_FILE)
-    local cmdline1="Add-Type -AssemblyName System.Windows.Forms"
-    local cmdline2="[System.Windows.Forms.Clipboard]::GetImage().Save"
-    local cmdline="$cmdline1;$cmdline2('$outfile')"
-    local rc=0
-    ERRMSG=$(powershell.exe -command "$cmdline" 2>&1 >/dev/null) || rc=$?
-    test $rc -eq 0 && return 0
-    echo "$ERRMSG" | grep -q "null-valued" && {
-        echo "No image data in clipboard." >&2
-        return 3
-    }
-    brief_error
-    return $rc
+    touch $OUT_FILE
+    local cmdline=$(cat << EOF
+        \$img = get-clipboard -format image
+        if (\$img -eq \$null) {
+            Write-Host "$MSG_NOIMGDATA"
+            Exit $RC_NOIMGDATA
+        }
+        echo "Saving image to $outfile"
+        \$img.save("$outfile")
+EOF
+    )
+
+    powershell.exe -command "$cmdline"
 }
 
 function wl_paste() {
     wl-paste --list-types | grep -q image/png || {
-        echo "No image data in clipboard." >&2
-        return 3
+        echo "$MSG_NOIMGDATA" >&2
+        return $RC_NOIMGDATA
     }
     wl-paste --no-newline --type image/png > $OUT_FILE
 }
 
 function xclip_paste() {
     xclip -selection clipboard -t TARGETS -o | grep -q image/png || {
-        echo "No image data in clipboard." >&2
-        return 3
+        echo "$MSG_NOIMGDATA" >&2
+        return $RC_NOIMGDATA
     }
     xclip -selection clipboard -t image/png -o > $OUT_FILE
 }
